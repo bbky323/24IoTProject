e55c70333b9ad5c058394b401c7fb7fcdd2924e5
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2024-12-01 05:02:46 +0900

    Update index.mjs

--- AWS_Lambda/index.mjs
+++ AWS_Lambda/index.mjs
@@ -7,9 +7,7 @@ import {
 } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
-
 const dynamo = DynamoDBDocumentClient.from(client);
-
 const tableName = "IoT_DB";
 
 export const handler = async (event, context) => {
@@ -27,32 +25,49 @@ export const handler = async (event, context) => {
         );
 
         if (!body.Items || body.Items.length === 0) {
-            return {
-              statusCode: 404,
-              body: JSON.stringify({ message: "No items found in the table" }),
-            };
+          return {
+            statusCode: 404,
+            body: JSON.stringify({ message: "No items found in the table" }),
+          };
         }
 
         const groupedData = body.Items.reduce((acc, item) => {
-            const cname = item.cname;
-            const ctemp = item.ctemp;
-            const chumid = item.chumid;
-  
-            if (!acc[cname]) {
-            acc[cname] = { ctempSum: 0, chumidSum: 0, count: 0 };
-            }
-  
-            acc[cname].ctempSum += ctemp;
-            acc[cname].chumidSum += chumid;
-            acc[cname].count += 1;
-  
-            return acc;
+          const cname = item.cname;
+          const ctemp = item.ctemp;
+          const chumid = item.chumid;
+
+          if (!acc[cname]) {
+            acc[cname] = { ctempSum: 0, chumidSum: 0, count: 0, firstCreated: item.id };
+          }
+
+          acc[cname].ctempSum += ctemp;
+          acc[cname].chumidSum += chumid;
+          acc[cname].count += 1;
+
+          return acc;
         }, {});
-  
-        body = Object.entries(groupedData).map(([cname, stats]) => ({
-            cname,
-            avgCtemp: Math.round(stats.ctempSum / stats.count),
-            avgChumid: Math.round(stats.chumidSum / stats.count),
+
+        const groupedArray = Object.entries(groupedData).map(([cname, stats]) => ({
+          cname,
+          avgCtemp: Math.round(stats.ctempSum / stats.count),
+          avgChumid: Math.round(stats.chumidSum / stats.count),
+          count: stats.count,
+          firstCreated: stats.firstCreated, 
+        }));
+
+        const sortedData = groupedArray
+          .sort((a, b) => {
+            if (b.count === a.count) {
+              return a.firstCreated < b.firstCreated ? -1 : 1; 
+            }
+            return b.count - a.count; 
+          })
+          .slice(0, 4);
+
+        body = sortedData.map((item) => ({
+          cname: item.cname,
+          avgCtemp: item.avgCtemp, 
+          avgChumid: item.avgChumid,
         }));
 
         break;
